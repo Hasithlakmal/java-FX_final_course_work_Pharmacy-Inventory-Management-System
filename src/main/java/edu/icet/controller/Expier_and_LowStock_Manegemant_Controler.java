ec2d79be0f34ba48e948dc8c854ab089c37dbd3f
Your NameHasithlakmal <you@example.comhasithlakmal0@gmail.com>
2025-11-12 16:13:13 +0530

    create expier and low Stock ui

---
+++ src/main/java/edu/icet/controller/Expier_and_LowStock_Manegemant_Controler.java
@@ -0,0 +1,98 @@
+package edu.icet.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class Expier_and_LowStock_Manegemant_Controler {
+
+    @FXML
+    private JFXButton btnRefresh;
+
+    @FXML
+    private JFXButton btnSupler;
+
+    @FXML
+    private JFXButton btn_Expier;
+
+    @FXML
+    private JFXButton btn_Stok;
+
+    @FXML
+    private JFXButton btn_add;
+
+    @FXML
+    private JFXButton btn_salse;
+
+    @FXML
+    private JFXButton btn_salseReport;
+
+    @FXML
+    private TableColumn<?, ?> columBarcode;
+
+    @FXML
+    private TableColumn<?, ?> columBrand;
+
+    @FXML
+    private TableColumn<?, ?> columExpiryDate;
+
+    @FXML
+    private TableColumn<?, ?> columName;
+
+    @FXML
+    private TableColumn<?, ?> columQuantity;
+
+    @FXML
+    private TableColumn<?, ?> columSupplier;
+
+    @FXML
+    private TableColumn<?, ?> columTelNo;
+
+    @FXML
+    private Label labelBilingNumber;
+
+    @FXML
+    private Label labelNetTotal;
+
+    @FXML
+    private TableView<?> tableAddMedi;
+
+    @FXML
+    void btnRefreshonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupler_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Expier_On_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Stok_OnActiopn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_addon_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_salseReport_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_salse_onAction(ActionEvent event) {
+
+    }
+
+}
