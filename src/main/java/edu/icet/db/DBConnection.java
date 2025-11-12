d0f829ceaa09bd7dedc8e2594376de72418751d7
Your NameHasithlakmal <you@example.comhasithlakmal0@gmail.com>
2025-11-12 23:32:26 +0530

    create db conection class

---
+++ src/main/java/edu/icet/db/DBConnection.java
@@ -0,0 +1,36 @@
+package edu.icet.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static  DBConnection  installDBConection;
+    private final Connection connection;
+
+    public DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pamacy","root","1234");
+    }
+
+    public  Connection getConnection(){
+
+        return  connection;
+
+    }
+
+
+    public static DBConnection  getDBConnection() throws SQLException {
+
+        if (installDBConection==null){
+
+            installDBConection=new DBConnection();
+
+        }
+
+        return installDBConection;
+
+    }
+
+
+}
