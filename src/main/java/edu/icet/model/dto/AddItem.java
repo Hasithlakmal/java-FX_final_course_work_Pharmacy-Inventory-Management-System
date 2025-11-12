f49532570ad173c3dc1d1d1de47a789ccb29bbfb
Your NameHasithlakmal <you@example.comhasithlakmal0@gmail.com>
2025-11-12 21:43:04 +0530

    creat  moidel dto  additem calss

---
+++ src/main/java/edu/icet/model/dto/AddItem.java
@@ -0,0 +1,23 @@
+package edu.icet.model.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class AddItem {
+
+    private String barcode;
+    private String name;
+    private String brand;
+    private LocalDate date;
+    private int quantity;
+    private double price;
+    private double sealing;
+
+}
