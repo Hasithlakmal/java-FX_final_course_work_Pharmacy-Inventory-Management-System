e27737aaa1af62dad87b05c9762bc31103498672
Your NameHasithlakmal <you@example.comhasithlakmal0@gmail.com>
2025-11-17 14:44:50 +0530

    create Suply dto class

---
+++ src/main/java/edu/icet/model/dto/Suply.java
@@ -0,0 +1,18 @@
+package edu.icet.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Suply {
+
+    private  String sumlyer;
+    private  String tel_no;
+    private  String bacode;
+
+
+}
