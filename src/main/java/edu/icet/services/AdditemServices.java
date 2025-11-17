0916ed6518dd69768c5e693a7ae10ce8e4a63402
Your NameHasithlakmal <you@example.comhasithlakmal0@gmail.com>
2025-11-18 00:12:03 +0530

    create getSelectedItam

--- src/main/java/edu/icet/services/AdditemServices.java
+++ src/main/java/edu/icet/services/AdditemServices.java
@@ -3,6 +3,8 @@ package edu.icet.services;
 import edu.icet.model.dto.AddItem;
 import javafx.collections.ObservableList;
 
+import java.sql.ResultSet;
+
 public interface AdditemServices {
 
     public int additem(AddItem item);
@@ -12,5 +14,7 @@ public interface AdditemServices {
     public int updateItem(AddItem item) ;
 
     public int deleteItem(String bacode, String name) ;
+
+    ResultSet getSelectedItam(String bacode) ;
 
 }
