54b2f293794e829df9842633919b8f6f75c9661a
Your NameHasithlakmal <you@example.comhasithlakmal0@gmail.com>
2025-11-18 00:14:17 +0530

    create BarcodeSerchOnAction  to get  nuregester   ans regester  suplayers  info

--- src/main/java/edu/icet/controller/SuplyeManagement_Controler.java
+++ src/main/java/edu/icet/controller/SuplyeManagement_Controler.java
@@ -1,14 +1,25 @@
 package edu.icet.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import edu.icet.model.dto.AddItem;
+import edu.icet.model.dto.Join_suplyerAndAdditem;
 import edu.icet.model.dto.ok;
+import edu.icet.services.AdditemServices;
+import edu.icet.services.SuplyeManagementServices;
+import edu.icet.services.impl.AdditemServicesImpl;
+import edu.icet.services.impl.SuplyeManagementServicesImpl;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import lombok.SneakyThrows;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,6 +27,8 @@ import java.util.ResourceBundle;
 public class SuplyeManagement_Controler implements Initializable {
 
 
+    SuplyeManagementServices suplyeManagementServices=new SuplyeManagementServicesImpl();
+    AdditemServices additemServices=new AdditemServicesImpl();
     ObservableList<ok> observableList = FXCollections.observableArrayList();
 
     @FXML
@@ -55,22 +68,40 @@ public class SuplyeManagement_Controler implements Initializable {
     private TableColumn<?, ?> columBrand;
 
     @FXML
+    private TableColumn<?, ?> columExpiryDate;
+
+    @FXML
     private TableColumn<?, ?> columName;
-
-    @FXML
-    private TableColumn<?, ?> columName11;
-
-    @FXML
-    private TableColumn<?, ?> columPrice;
 
     @FXML
     private TableColumn<?, ?> columQuantity;
 
     @FXML
-    private TableColumn<?, ?> columSelingPrice;
+    private TableColumn<?, ?> columSupplier;
 
     @FXML
-    private TableView<ok> tableAddMedi;
+    private TableColumn<?, ?> columTel_No;
+    @FXML
+    private TableView<Join_suplyerAndAdditem> table;
+
+
+    @FXML
+    private JFXTextField textBarcode;
+
+    @FXML
+    private JFXTextField textBrand;
+
+    @FXML
+    private JFXTextField textQuantity;
+
+    @FXML
+    private JFXTextField textSupplier;
+
+    @FXML
+    private JFXTextField textTel;
+
+    @FXML
+    private DatePicker DatePiker;
 
     @FXML
     void btnAddonAction(ActionEvent event) {
@@ -123,8 +154,75 @@ public class SuplyeManagement_Controler implements Initializable {
 
     }
 
+    @SneakyThrows
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        columBarcode.setCellValueFactory(new PropertyValueFactory<>("barcode"));
+        columName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        columBrand.setCellValueFactory(new PropertyValueFactory<>("brand"));
+        columSupplier.setCellValueFactory(new PropertyValueFactory<>("sumlyer"));
+        columQuantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        columExpiryDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        columTel_No.setCellValueFactory(new PropertyValueFactory<>("tel_no"));
+
+
+        loadItamTabale();
+
+
+    }
+
+
+    public void  loadItamTabale(){
+
+        table.setItems(suplyeManagementServices.getallItam());
+
+    }
+
+
+    @FXML
+    void BarcodeSerchOnAction(KeyEvent event) {
+
+        String text = textBarcode.getText();
+
+        for ( Join_suplyerAndAdditem  joinSuplyerAndAdditem  : suplyeManagementServices.getallItam()) {
+
+            if (joinSuplyerAndAdditem.getBarcode().equals(text)){
+
+
+                textBrand.setText(joinSuplyerAndAdditem.getBrand());
+                DatePiker.setValue(joinSuplyerAndAdditem.getDate());
+                textQuantity.setText(String.valueOf(joinSuplyerAndAdditem.getQuantity()));
+                textSupplier.setText(joinSuplyerAndAdditem.getSumlyer());
+                textTel.setText(joinSuplyerAndAdditem.getTel_no());
+
+
+            }else {
+
+
+
+                for (AddItem Item : additemServices.getallItam()) {
+
+                    if (Item.getBarcode().equals(text)){
+
+
+                        textBrand.setText(Item.getBrand());
+                        DatePiker.setValue(Item.getDate());
+                        textQuantity.setText(String.valueOf(Item.getQuantity()));
+
+
+                    }
+
+
+
+                }
+
+            }
+
+
+        }
+
 
 
 
