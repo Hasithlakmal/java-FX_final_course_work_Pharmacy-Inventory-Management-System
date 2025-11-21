bf5dc5a9fb23709969eebfbd4711b23566105621
Your NameHasithlakmal <you@example.comhasithlakmal0@gmail.com>
2025-11-21 16:53:21 +0530

    create sealseReport dto class

---
+++ src/main/java/edu/icet/model/dto/sealseReport.java
@@ -0,0 +1,23 @@
+package edu.icet.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+
+public class sealseReport {
+
+    private String barcode;
+    private String name;
+    private String brand;
+    private double  cost;
+    private int quantity;
+    private double selse;
+    private double Profit;
+
+
+}
