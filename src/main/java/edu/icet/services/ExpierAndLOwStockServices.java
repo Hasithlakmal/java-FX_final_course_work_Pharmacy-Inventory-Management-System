fd63bf6997c502a4ab5d875757a917fd8cb685de
Your NameHasithlakmal <you@example.comhasithlakmal0@gmail.com>
2025-11-21 13:20:03 +0530

    creat  table lode method and chake exper date and low quantity methode

--- src/main/java/edu/icet/services/ExpierAndLOwStockServices.java
+++ src/main/java/edu/icet/services/ExpierAndLOwStockServices.java
@@ -1,8 +1,11 @@
 package edu.icet.services;
 
+import edu.icet.model.dto.LowAndExpier;
+import javafx.collections.ObservableList;
+
 public interface ExpierAndLOwStockServices {
 
 
-
+    public ObservableList<LowAndExpier> TableLoadDetails();
 
 }
--- src/main/java/edu/icet/services/impl/ExpierAndLOwStockServicesImpl.java
+++ src/main/java/edu/icet/services/impl/ExpierAndLOwStockServicesImpl.java
@@ -1,4 +1,108 @@
 package edu.icet.services.impl;
 
-public class ExpierAndLOwStockServicesImpl {
+import edu.icet.model.dto.Join_suplyerAndAdditem;
+import edu.icet.model.dto.LowAndExpier;
+import edu.icet.services.AdditemServices;
+import edu.icet.services.ExpierAndLOwStockServices;
+import edu.icet.services.SuplyeManagementServices;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.time.LocalDate;
+
+public class ExpierAndLOwStockServicesImpl implements ExpierAndLOwStockServices {
+
+    private AdditemServices additemServices= new AdditemServicesImpl();
+    private SuplyeManagementServices suplyeManagementServices =new SuplyeManagementServicesImpl();
+    
+
+
+    private String date = "";
+
+    private int LowNumber = 5;
+
+
+    @Override
+    public ObservableList<LowAndExpier> TableLoadDetails(){
+
+        ObservableList<LowAndExpier> lowAndExpiers=FXCollections.observableArrayList();
+        ObservableList<Join_suplyerAndAdditem> combinedList = FXCollections.observableArrayList();
+
+        combinedList.addAll(suplyeManagementServices.regesterSuplayes());
+        combinedList.addAll(suplyeManagementServices.getUnRegisterSuplers());
+
+        for (Join_suplyerAndAdditem exAndLo : combinedList) {
+
+            lowAndExpiers.add(new LowAndExpier(
+
+                    exAndLo.getBarcode(),
+                    exAndLo.getName(),
+                    exAndLo.getBrand(),
+                    exAndLo.getSumlyer(),
+                    exAndLo.getQuantity(),
+                    ChakExperStatus(String.valueOf(exAndLo.getDate())),
+                    ChakLowStockStatus(exAndLo.getQuantity()),
+                    exAndLo.getTel_no()
+
+            ));
+
+        }
+
+        return lowAndExpiers;
+
+    }
+
+
+
+
+
+  private String ChakExperStatus(String localDate) {
+
+        String dateStr = localDate;
+        String ExpierDate = dateStr.replace("-", "");
+        int intExpierDate = Integer.parseInt(ExpierDate);
+
+
+        int intTOday =setDateString();
+
+        String ExP = "";
+
+        if (intExpierDate - intTOday == 0) {
+
+            ExP = "Expired";
+
+        } else if (intExpierDate - intTOday < 0) {
+
+            ExP = "EXP " + (intExpierDate - intTOday) + "Day ago";
+
+
+        } else if (intExpierDate - intTOday > 0) {
+
+            ExP = "EXP With In " + (intExpierDate - intTOday) + "Day";
+
+        }
+
+
+        return ExP;
+
+    }
+
+
+    private String ChakLowStockStatus(int Quty) {
+
+        return Quty>LowNumber ? ""+Quty : "Low_Stock";
+    }
+
+
+    private int setDateString(){
+
+        String dateStr =String.valueOf(LocalDate.now());
+        String Today = dateStr.replace("-", "");
+
+        return Integer.parseInt(Today);
+
+
+    }
+
+
 }
